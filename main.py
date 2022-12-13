751874451be137492e8a27f3e69a6d89ba8b2c2a
Nada Atieh <nada.s.atieh@gmail.com>
2022-12-13 07:49:17 -0600

    I wanted to see how far I could get on my own without any help however it seems the user input response is not connecting to the code and translating to any meaningful changes to move it along

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
+import random
+
 destinations = ['New York', 'London', 'Rome', 'Barcelona']
 restaurants = ['JJs Pizza', 'Gordos', 'Le Calandre', 'Alkimia']
-transportation = ['Plane,' 'Cruise', 'Train', 'Car']
+transportation = ['Plane', 'Cruise', 'Train', 'Car']
 entertainment = ['Knicks Game', 'Theater', 'Ballet', 'Art show']
+
+for destination in destinations:
+    print(destination)
+for restaurant in restaurants:
+    print(restaurant)
+for transport in transportation:
+    print(transport)
+for mode in entertainment:
+    print(mode)
+
+print = input('Are you satisfied with your trip?' 'Y/N')
+
+random = destination = random.choice(destinations)
+random = restaurant =random.choice(restaurants)
+random = transport = random.choice(transportation)
+random = mode = random.choice(entertainment)
+user_response = 'N'
+
+if user_response != 'Y':
+    print = random.choice (destination, restaurant, transport, mode)
+
+if user_response == 'Y':
+    print('Enjoy your trip!')
